8a3a2c5a4b5b62c5599ef2a5db6619e9d62e0f92
Ben <46476247+hexagonatron@users.noreply.github.com>
2020-05-23 22:30:42 +0930

    sequelize init

--- controllers/routes.js
+++ controllers/routes.js
@@ -3,6 +3,17 @@ const path = require("path");
 
 const router = express.Router();
 
+//Render index with all burgers in DB
+router.get("/", (req, res) => {
+    Burger.all((data) => {
+        const allBurgers = {
+            burgers: data
+        }
+
+        res.render("index", allBurgers);
+    });
+});
+
 router.get("/api/burgers", (req, res) => {
 
 });
