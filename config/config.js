29d5cf751fd1f3d3a40130802d9d3f0af0a382b3
Ben <46476247+hexagonatron@users.noreply.github.com>
2020-05-25 01:13:03 +0930

    config files

--- config/config.js
+++ config/config.js
@@ -10,18 +10,20 @@ module.exports = {
     "operatorsAliases": false
   },
   "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
+    "username": process.env.MYSQL_USER_NAME,
+    "password": process.env.MYSQL_PASSWORD,
+    "database": "burgerdb",
+    "host": process.env.MYSQL_HOST,
+    "port": process.env.MYSQL_PORT,
     "dialect": "mysql",
     "operatorsAliases": false
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "username": process.env.MYSQL_USER_NAME,
+    "password": process.env.MYSQL_PASSWORD,
+    "database": "burgerdb",
+    "host": process.env.MYSQL_HOST,
+    "port": process.env.MYSQL_PORT,
     "dialect": "mysql",
     "operatorsAliases": false
   }
